45a188cb21c085761f6cc82f27a54180ee343cb8
Александр Чазов <chazovby@gmail.com>
2022-07-02 17:11:02 +0300

    feat: SQL Basics: Mod

--- oppositeNumber.sql
+++ oppositeNumber.sql
@@ -7,6 +7,10 @@ SELECT -number FROM opposite;
 
 SELECT * FROM myTable;
 
+-- или проще
+
+Select -number AS res FROM opposite;
+
 
 --Very simple, given an integer or a floating-point number, find its opposite.
 
