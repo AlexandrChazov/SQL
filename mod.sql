bb36fddaa5494021912699b2ff90116eeb0367bc
Александр Чазов <chazovby@gmail.com>
2022-07-02 17:16:36 +0300

    feat: SQL Basics: Mod comment

--- mod.sql
+++ mod.sql
@@ -8,3 +8,6 @@ SELECT mod(number1, number2) AS mod FROM decimals
 --number1
 --number2
 --Return a table with one column (mod) which is the output of number1 modulus number2.
+
+-- функция mod это остаток от деления, например mod(8,3) = 2  mod(11,2) = 1
+-- это тоже самое что и функция % в js
